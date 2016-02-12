7170cb8dfa889118f7fcbdc0e9668ff1130677da
Hendrik Lücke-Tieke <helt@outlook.com>
2016-02-12 01:29:34 +0100

    Add info about the injected service isntance

--- ltde.pets.nlp.uima-spring/src/main/java/ltde/pets/nlp/uimaSpring/autowired/MyAutowiredAnalysisEngine.java
+++ ltde.pets.nlp.uima-spring/src/main/java/ltde/pets/nlp/uimaSpring/autowired/MyAutowiredAnalysisEngine.java
@@ -12,22 +12,27 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Created by hatieke on 2016-02-11.
  */
 public class MyAutowiredAnalysisEngine extends JCasAnnotator_ImplBase {
-    private static final Logger LOG = LoggerFactory.getLogger(MyAutowiredAnalysisEngine.class);
+  private static final Logger LOG = LoggerFactory.getLogger(MyAutowiredAnalysisEngine.class);
 
-    @Autowired
-    private ExampleService srv;
+  @Autowired
+  private ExampleService srv;
 
-    public final static String ARBITRARY_BUT_CERTAINLY_NOT_OVERLOADED_NAME = "arbitraryButCertainlyNotOverloadedName";
-    @ConfigurationParameter(name = ARBITRARY_BUT_CERTAINLY_NOT_OVERLOADED_NAME)
-    private String randomNameThatIsNotAmbigue;
+  public final static String ARBITRARY_BUT_CERTAINLY_NOT_OVERLOADED_NAME =
+      "arbitraryButCertainlyNotOverloadedName";
+  @ConfigurationParameter(name = ARBITRARY_BUT_CERTAINLY_NOT_OVERLOADED_NAME)
+  private String randomNameThatIsNotAmbigue;
 
-    public MyAutowiredAnalysisEngine() {
-        LOG.info("Instantiating " + MyAutowiredAnalysisEngine.class.getCanonicalName() + " and got randomNameThatIsNotAmbigue '" + randomNameThatIsNotAmbigue +"'");
-    }
+  public MyAutowiredAnalysisEngine() {
+    LOG.info("Instantiating " + MyAutowiredAnalysisEngine.class.getCanonicalName()
+        + " and got randomNameThatIsNotAmbigue '" + randomNameThatIsNotAmbigue + "'");
+  }
 
-    @Override
-    public void process(JCas aJCas) throws AnalysisEngineProcessException {
-        String documentText = aJCas.getDocumentText();
-        LOG.info(String.format("AE %s (hash %s) processes cas-id %s with document %s", this.randomNameThatIsNotAmbigue, this.hashCode(), aJCas.hashCode(), documentText.substring(0, Math.min(100, documentText.length()))));
-    }
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    String documentText = aJCas.getDocumentText();
+    LOG.info(srv.get());
+    LOG.info(String.format("AE %s (hash %s) processes cas-id %s with document %s", this
+        .randomNameThatIsNotAmbigue, this.hashCode(), aJCas.hashCode(), documentText.substring(0,
+        Math.min(100, documentText.length()))));
+  }
 }
