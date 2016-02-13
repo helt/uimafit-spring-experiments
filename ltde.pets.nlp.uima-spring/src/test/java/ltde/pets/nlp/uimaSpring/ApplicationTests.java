f52a3ae7807164f3beb63a257270941b8b8483b6
Hendrik Lücke-Tieke <helt@outlook.com>
2016-02-13 21:07:46 +0100

    code cleanup

--- ltde.pets.nlp.uima-spring/src/test/java/ltde/pets/nlp/uimaSpring/ApplicationTests.java
+++ ltde.pets.nlp.uima-spring/src/test/java/ltde/pets/nlp/uimaSpring/ApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
